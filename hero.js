bcb7cc966c46aced11e39585cab12be3de0cf257
Namaste-React <triptasharma8000@gmail.com>
2025-04-10 17:17:58 +0530

    Initial commit

---
+++ hero.js
@@ -0,0 +1,62 @@
+// * js me code line by line likhna hai
+
+var a = 20;
+var b = 30;
+var c = 40;
+console.log(a);
+console.log(b);
+console.log(c);
+
+// * 
+// 1. console, error & prompt are the parts of the browser.
+// 2. console is used to print the output and error.
+// 3. error is use to show the error in the code.
+// 4. prompts used to take input from the user.
+// 5. prompts for taking inputs only.
+// 6. alert for taking input and showing output.
+
+
+console.log("Hello");
+console.warn("this is a warning");
+console.error("this is an error");
+
+
+let hero = prompt("this is a prompt");
+console.log(hero);
+alert("this is an alert");
+
+
+
+// * JS Variable
+
+
+//Let
+
+
+
+
+
+
+
+//Const
+
+
+
+
+
+//Var
+// global variable
+//ES-6 or older versions
+//Hoisting supported
+//closures supported
+//not advised to use
+// value assign to var can be modified
+//for ex - var a = 10;
+// function sum(a){
+//var a = 20;
+//}
+//console.log(a);
+
+
+
+//
